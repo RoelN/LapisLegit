07dec3b0cd2a1ae5d48da743b1dac6c2f852473a
Roel Nieskens <roel@pixelambacht.nl>
2016-11-04 15:23:14 +0100

    Build with proper name table, based on EmojiOne's yaml file

--- buildfont.sh
+++ buildfont.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/code/scfbuild/bin/scfbuild -v -o dist/LapisLegit.ttf -g ./src/svg -s ./src/svg
+~/code/scfbuild/bin/scfbuild -c scfbuild-osx.yml -v -o dist/LapisLegit.ttf -g ./src/svg -s ./src/svg
 ./generatetestpage.py > testpage.html
