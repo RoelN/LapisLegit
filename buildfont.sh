9d57c379734070f0591c0014009ff535c5c5a872
Roel Nieskens <roel@pixelambacht.nl>
2017-01-18 22:11:45 +0100

    Add CPAL table and test glyph

--- buildfont.sh
+++ buildfont.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# Create font from SVG files
 ~/code/scfbuild/bin/scfbuild -c scfbuild-osx.yml -v -o dist/LapisLegit.ttf -g ./src/svg -s ./src/svg
+
+# Add CPAL table
+ttx -m dist/LapisLegit.ttf src/cpal.ttx
+rm dist/LapisLegit.ttf
+mv src/cpal.ttf dist/LapisLegit.ttf
+
 ./generatetestpage.py > testpage.html
